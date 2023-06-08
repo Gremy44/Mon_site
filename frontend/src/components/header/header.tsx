daaa32c8bf5b0465bc7dccabd258d81dedec1175
Remy Gosselin <gosselin.remy@gmail.com>
2023-06-08 16:51:33 +0400

    add react

---
+++ frontend/src/components/header/header.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>Header : </h1>
+      <div className="menu">
+        <ul>
+          <li>Bienvenue</li>
+          <li>HTML/CSS/TS</li>
+          <li>App Web</li>
+          <li>Base de données</li>
+          <li>API REST</li>
+          <li>Versionning</li>
+          <li>Déploiement</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
